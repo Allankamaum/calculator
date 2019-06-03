6eb12bcdf05a51200e513240ae2c117815f104bb
Allan Kamau <akamau41@gmail.com>
2019-06-03 07:37:44 +0300

    add both formModule and Reactive form module to project file

--- Quote/src/app/app.module.ts
+++ Quote/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
@@ -13,6 +15,7 @@ import { QuotesComponent } from './quotes/quotes.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
