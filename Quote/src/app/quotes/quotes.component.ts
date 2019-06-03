2c74f44c419499391621b8e46aa66488c3592c12
Allan Kamau <akamau41@gmail.com>
2019-06-03 07:32:36 +0300

    setting funnctions for the Quote and name section

--- Quote/src/app/quotes/quotes.component.ts
+++ Quote/src/app/quotes/quotes.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+// import { Quote } from '../quote'
+
 
 @Component({
   selector: 'app-quotes',
@@ -7,18 +9,25 @@ import { FormControl } from '@angular/forms'
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  @Output() isComplete = new EventEmitter<boolean>();
 
-name = new FormControl('')
-quote =new FormControl('')
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+
+  name = new FormControl('')
+  quote = new FormControl('')
   constructor() { }
 
-  updateName(){
+  updateName() {
     this.name.setValue('talk');
-    this.quote.setValue('talk')
+    // this.quote.setValue('talk')
   }
-  // updateQuote(){
-  //   this.quote.setValue('talk')
-  // }
+  updateQuote() {
+    this.quote.setValue('tal')
+
+  }
+
 
   ngOnInit() {
   }
